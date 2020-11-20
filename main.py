4f4d4d4e09d3d1c639cdbba642cd97645e52781f
Alex <alexmal2804@gmail.com>
2020-11-20 18:05:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import numpy as np
+from matplotlib import pyplot as plt
+
+ys = 200 + np.random.randn(100)
+x = [x for x in range(len(ys))]
+
+plt.plot(x, ys, '-')
+plt.fill_between(x, ys, 195, where=(ys > 195), facecolor='g', alpha=0.6)
+
+plt.title("Sample Visualization")
+plt.show()
